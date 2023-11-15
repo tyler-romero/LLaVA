7cd7404b47e6047ace2a860e93e2f355f2bf3570
Tyler Romero <tyler@groundlight.ai>
2023-11-14 16:47:39 -0800

    Revert "predict.py: streaming output"
    This reverts commit 86efd49aba76ce89b9d9a7f03c25386f78d4a9ad.

--- predict.py
+++ predict.py
@@ -4,20 +4,21 @@ from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN
 from llava.conversation import conv_templates, SeparatorStyle
 from llava.model.builder import load_pretrained_model
 from llava.utils import disable_torch_init
-from llava.mm_utils import tokenizer_image_token
-from transformers.generation.streamers import TextIteratorStreamer
+from llava.mm_utils import tokenizer_image_token, KeywordsStoppingCriteria
 
 from PIL import Image
 
 import requests
+import os
+import subprocess
+import time
 from io import BytesIO
 
-from cog import BasePredictor, Input, Path, ConcatenateIterator
-import time
-import subprocess
-from threading import Thread
+import requests
+import torch
+from cog import BasePredictor, Input, Path
 
-import os
+
 os.environ["HUGGINGFACE_HUB_CACHE"] = os.getcwd() + "/weights"
 
 # url for the weights mirror
@@ -38,18 +39,15 @@ weights = [
             "special_tokens_map.json",
             "tokenizer.model",
             "tokenizer_config.json",
-        ]
+        ],
     },
     {
         "dest": "openai/clip-vit-large-patch14-336",
         "src": "clip-vit-large-patch14-336/ce19dc912ca5cd21c8a653c79e251e808ccabcd1",
-        "files": [
-            "config.json",
-            "preprocessor_config.json",
-            "pytorch_model.bin"
-        ],
-    }
+        "files": ["config.json", "preprocessor_config.json", "pytorch_model.bin"],
+    },
 ]
+
 
 def download_json(url: str, dest: Path):
     res = requests.get(url, allow_redirects=True)
@@ -58,6 +56,7 @@ def download_json(url: str, dest: Path):
             f.write(res.content)
     else:
         print(f"Failed to download {url}. Status code: {res.status_code}")
+
 
 def download_weights(baseurl: str, basedest: str, files: list[str]):
     basedest = Path(basedest)
@@ -75,81 +74,111 @@ def download_weights(baseurl: str, basedest: str, files: list[str]):
                 subprocess.check_call(["pget", url, str(dest)], close_fds=False)
     print("downloading took: ", time.time() - start)
 
+
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
         for weight in weights:
             download_weights(weight["src"], weight["dest"], weight["files"])
         disable_torch_init()
-    
-        self.tokenizer, self.model, self.image_processor, self.context_len = load_pretrained_model("liuhaotian/llava-v1.5-13b", model_name="llava-v1.5-13b", model_base=None, load_8bit=False, load_4bit=False)
+
+        (
+            self.tokenizer,
+            self.model,
+            self.image_processor,
+            self.context_len,
+        ) = load_pretrained_model(
+            "liuhaotian/llava-v1.5-13b",
+            model_name="llava-v1.5-13b",
+            model_base=None,
+            load_8bit=False,
+            load_4bit=False,
+        )
 
     def predict(
         self,
         image: Path = Input(description="Input image"),
         prompt: str = Input(description="Prompt to use for text generation"),
-        top_p: float = Input(description="When decoding text, samples from the top p percentage of most likely tokens; lower to ignore less likely tokens", ge=0.0, le=1.0, default=1.0),
-        temperature: float = Input(description="Adjusts randomness of outputs, greater than 1 is random and 0 is deterministic", default=0.2, ge=0.0),
-        max_tokens: int = Input(description="Maximum number of tokens to generate. A word is generally 2-3 tokens", default=1024, ge=0),
-    ) -> ConcatenateIterator[str]:
+        top_p: float = Input(
+            description="When decoding text, samples from the top p percentage of most likely tokens; lower to ignore less likely tokens",
+            ge=0.0,
+            le=1.0,
+            default=1.0,
+        ),
+        temperature: float = Input(
+            description="Adjusts randomness of outputs, greater than 1 is random and 0 is deterministic",
+            default=0.2,
+            ge=0.0,
+        ),
+        max_tokens: int = Input(
+            description="Maximum number of tokens to generate. A word is generally 2-3 tokens",
+            default=1024,
+            ge=0,
+        ),
+    ) -> str:
         """Run a single prediction on the model"""
-    
+
         conv_mode = "llava_v1"
         conv = conv_templates[conv_mode].copy()
-    
+
         image_data = load_image(str(image))
-        image_tensor = self.image_processor.preprocess(image_data, return_tensors='pt')['pixel_values'].half().cuda()
-    
+        image_tensor = (
+            self.image_processor.preprocess(image_data, return_tensors="pt")[
+                "pixel_values"
+            ]
+            .half()
+            .cuda()
+        )
+
         # loop start
-    
+
         # just one turn, always prepend image token
-        inp = DEFAULT_IMAGE_TOKEN + '\n' + prompt
+        inp = DEFAULT_IMAGE_TOKEN + "\n" + prompt
         conv.append_message(conv.roles[0], inp)
 
         conv.append_message(conv.roles[1], None)
         prompt = conv.get_prompt()
-    
-        input_ids = tokenizer_image_token(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+
+        input_ids = (
+            tokenizer_image_token(
+                prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt"
+            )
+            .unsqueeze(0)
+            .cuda()
+        )
         stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
         keywords = [stop_str]
-        streamer = TextIteratorStreamer(self.tokenizer, skip_prompt=True, timeout=20.0)
-    
+
+        stopping_criteria = KeywordsStoppingCriteria(
+            keywords, self.tokenizer, input_ids
+        )
+
         with torch.inference_mode():
-            thread = Thread(target=self.model.generate, kwargs=dict(
-                inputs=input_ids,
+            output_ids = self.model.generate(
+                input_ids,
                 images=image_tensor,
                 do_sample=True,
                 temperature=temperature,
                 top_p=top_p,
                 max_new_tokens=max_tokens,
-                streamer=streamer,
-                use_cache=True))
-            thread.start()
-            # workaround: second-to-last token is always " "
-            # but we want to keep it if it's not the second-to-last token
-            prepend_space = False
-            for new_text in streamer:
-                if new_text == " ":
-                    prepend_space = True
-                    continue
-                if new_text.endswith(stop_str):
-                    new_text = new_text[:-len(stop_str)].strip()
-                    prepend_space = False
-                elif prepend_space:
-                    new_text = " " + new_text
-                    prepend_space = False
-                if len(new_text):
-                    yield new_text
-            if prepend_space:
-                yield " "
-            thread.join()
-    
+                use_cache=True,
+                stopping_criteria=[stopping_criteria],
+            )
+
+        outputs = self.tokenizer.decode(
+            output_ids[0, input_ids.shape[1] :], skip_special_tokens=True
+        ).strip()
+        conv.messages[-1][-1] = outputs
+
+        if outputs.endswith(stop_str):
+            outputs = outputs[: -len(stop_str)].strip()
+        return outputs
+
 
 def load_image(image_file):
-    if image_file.startswith('http') or image_file.startswith('https'):
+    if image_file.startswith("http") or image_file.startswith("https"):
         response = requests.get(image_file)
-        image = Image.open(BytesIO(response.content)).convert('RGB')
+        image = Image.open(BytesIO(response.content)).convert("RGB")
     else:
-        image = Image.open(image_file).convert('RGB')
+        image = Image.open(image_file).convert("RGB")
     return image
-
